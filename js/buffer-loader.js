5aa319e1a080a62789b590c5397d521521439211
shinnn <dminor.sw@gmail.com>
2013-05-15 21:25:44 +0900

    improve buffer-loader.js
    Base64エンコーディングを使わなくてもiOSでオーディオデータをバッファできる方法が見つかったので、そちらに切り替える

--- js/buffer-loader.js
+++ js/buffer-loader.js
@@ -9,66 +9,55 @@ function BufferLoader(context,urlList,callback){
 
 (function(){
   var is_iOS = navigator.userAgent.indexOf('like Mac OS X') !== -1;
-  
   var loader;
+
+  BufferLoader.prototype.loadBuffer = function(url, index){
+    // Load buffer asynchronously
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    loader = this;
+    request.onload = function(){
+      // Asynchronously decode the audio file data in request.response
+      webAudioDecode(getArrayBuffer(request.response), index);
+    };
   
-  // http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-  function ab2str(buf){
-    // String.fromCharCode.apply(null, new Uint8Array(buf)) だと
-    // 'Maximum call stack size exceeded' が発生するため、下記のように処理する
+    request.onerror = xhrError;
+    request.send();
+  };
+  
+  var getArrayBuffer;
+  if(is_iOS){
+    // http://updates.html5rocks.com/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
+    var ab2str = function(buf){
+      // String.fromCharCode.apply(null, new Uint8Array(buf)) だと
+      // 'Maximum call stack size exceeded' が発生するため、下記のように処理する
         
-    var arr = new Uint8Array(buf);
-    var str = '';
-    for(var i=0, len=arr.length; i<len; i++){
-      str += String.fromCharCode(arr[i]);
-    }
-    return str;
-  }
-
-  function str2ab(str){
-    var buf = new ArrayBuffer(str.length * Uint8Array.BYTES_PER_ELEMENT); // BYTES_... = 1
-    var bufView = new Uint8Array(buf);
-    for (var i=0, len=str.length; i<len; i++){
-      bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-  }
-  
-  BufferLoader.prototype.loadBuffer = function(url, index){
-    // Load buffer asynchronously
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    loader = this;
-    request.onload = function(){
-      // Asynchronously decode the audio file data in request.response
-      
-      webAudioDecode.call(this, request.response, index);
+      var arr = new Uint8Array(buf);
+      var str = '';
+      for(var i=0, len=arr.length; i<len; i++){
+        str += String.fromCharCode(arr[i]);
+      }
+      return str;
     };
-  
-    request.onerror = xhrError;
-  
-    request.send();
-  };
 
-  BufferLoader.prototype.loadBuffer = function(url, index){
-    // Load buffer asynchronously
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    loader = this;
-    request.onload = function(){
-      // Asynchronously decode the audio file data in request.response
-      webAudioDecode(
-        is_iOS? str2ab(ab2str(request.response)): request.response,
-        index
-      );
+    var str2ab = function(str){
+      var buf = new ArrayBuffer(str.length * Uint8Array.BYTES_PER_ELEMENT); // BYTES_... = 1
+      var bufView = new Uint8Array(buf);
+      for (var i=0, len=str.length; i<len; i++){
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
     };
-  
-    request.onerror = xhrError;
-  
-    request.send();
-  };
+    
+    getArrayBuffer = function(res){
+      return str2ab(ab2str(res));
+    };
+  }else{
+    getArrayBuffer = function(res){
+      return res;
+    };
+  }
   
   function webAudioDecode(arrayBuffer, index){
     /*
